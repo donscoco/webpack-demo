0f2001b19a6780d9b2d74466d8a0156de7379a5c
donscoco <286563721@qq.com>
2019-05-28 17:52:34 +0800

    fix:css

--- src/page/about/index.js
+++ src/page/about/index.js
@@ -2,10 +2,9 @@
 * @Author: donscoco
 * @Date:   2019-04-08 09:27:17
 * @Last Modified by:   donscoco
-* @Last Modified time: 2019-05-28 17:49:39
+* @Last Modified time: 2019-05-28 17:52:24
 */
 'use strict';
-require('./index.css');
 require('page/common/nav/index.js');
 require('page/common/header/index.js');
 require('page/common/footer/index.js');
